1a3cc280c554eb5a90b07ae0656a470fb41b6f79
Patrik Moreira <moreirazezanegra@outlook.com>
2024-12-01 11:17:29 -0300

    adicionando pointer e rotas nos buttons

--- totem/src/components/ButtonAmbientes.jsx
+++ totem/src/components/ButtonAmbientes.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ambientes from '../assets/ambientes.png'
+import './componentsStyles/ButtonAmbientes.css'
+import { useNavigate } from "react-router-dom";
+
 function ButtonAmbientes(){
+
+    const navigate = useNavigate();
+
+    const handleImageClick = () => {
+      navigate("/ambientes-coletivos");
+    };
+
     return(
         <>
-                <img className="button" src={ambientes} alt="botao de ambientes coletivos" />
+                <img onClick={handleImageClick} className="button" src={ambientes} alt="botao de ambientes coletivos" />
         </>
     )
 }
--- totem/src/components/ButtonEncontreSala.jsx
+++ totem/src/components/ButtonEncontreSala.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import './componentsStyles/ButtonEncontreSala.css'
 import encontreSala from '../assets/encontre.png' 
+import { useNavigate } from "react-router-dom";
+
 function ButtonEncontreSala(){
+
+    const navigate = useNavigate();
+
+    const handleImageClick = () => {
+      navigate("/buscasala");
+    };
+
     return(
         <>
-            <img className="button" src={encontreSala} alt="botao de encontre sua sala" />
+            <img onClick={handleImageClick} className="button" src={encontreSala} alt="botao de encontre sua sala" />
         </>
     )
 }
