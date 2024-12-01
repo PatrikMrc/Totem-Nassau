1db1bf1bf05c0b965e4fb284c8995da23dd94241
Dionildo <dionildo.melo1@gmail.com>
2024-12-01 12:12:14 -0300

    alteraçoes no css

--- totem/src/pages/Busca.jsx
+++ totem/src/pages/Busca.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import '../styles/Buscabutton.css';
 import craImg from "../assets/cra.svg";
 import coordenadoriaImg from "../assets/coordenadoria.svg";
+import Title from "../components/TitleBusca"; // Ajuste o caminho se necessário
 
 function BotoesComImagens() {
   return (
-    <div className="button-container">
-      <button className="image-button">
-        <img src={craImg} alt="CRA" className="button-image" />
-        CRA
-      </button>
-      <button className="image-button">
-        <img src={coordenadoriaImg} alt="Coordenadoria" className="button-image" />
-        Coordenadoria
-      </button>
+    <div>
+      {/* Adiciona o título antes do conteúdo */}
+      <Title />
+      
+      <div className="button-container">
+        <button className="image-button">
+          <img src={craImg} alt="CRA" className="button-image" />
+          CRA
+        </button>
+        <button className="image-button">
+          <img src={coordenadoriaImg} alt="Coordenadoria" className="button-image" />
+          Coordenadoria
+        </button>
+      </div>
     </div>
   );
 }
